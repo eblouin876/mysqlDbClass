8eee34db40aa0a4a5bf64120eca2d00fb1600e53
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-04-05 10:48:59 -0700

    updated port and host

--- database.js
+++ database.js
@@ -153,8 +153,8 @@ class Database {
   connect() {
     return new Promise((resolve, reject) => {
       this.connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
+        host: process.env.HOST || "localhost",
+        port: process.env.PORT || 8080,
         user: process.env.DB_USER,
         password: process.env.DB_PASS
       });
